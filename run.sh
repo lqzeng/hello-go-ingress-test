6f0d375b48557668bb9b8e700c26a0f853493562
Lucas Zeng <lucasqzeng@gmail.com>
2022-11-14 14:33:47 +1100

    adding script to run in pipeline

---
+++ run.sh
@@ -0,0 +1,22 @@
+echo "STARTING SCRIPT"
+
+echo "creating cluster"
+kind create cluster --config=kind-ingress.yaml
+
+echo "loading basic go hello docker image"
+kind load docker-image hello-go
+
+echo "applying nginx ingress"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+echo "applying ingress usage"
+kubectl apply -f ingress-usage.yaml
+
+echo "checking deployment"
+kubectl get all
+
+echo "checking apps are running"
+localhost/foo
+localhost/bar
+
+echo "SCRIPT FINISHED"
