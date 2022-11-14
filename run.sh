06541b77347bbe1d01ac0e750dc5112fddb73030
Lucas Zeng <lucasqzeng@gmail.com>
2022-11-14 17:28:03 +1100

    asdf

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ echo "STARTING SCRIPT"
 echo $HOME 
 echo $PATH
 
-echo "deleting webhook validation"
-kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
+# echo "deleting webhook validation"
+# kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
 
 # echo "--- list docker images"
 # docker images
@@ -30,8 +30,8 @@ kind load docker-image hello-go
 echo "applying nginx ingress"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
-echo "deleting webhook validation"
-kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
+# echo "deleting webhook validation"
+# kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
 
 echo "applying ingress usage"
 kubectl apply -f ingress-usage.yaml
