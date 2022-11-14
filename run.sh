a146b06a5e5e30e37d1c4c12af9a87a47acd345e
Lucas Zeng <lucasqzeng@gmail.com>
2022-11-14 16:23:13 +1100

    s

--- run.sh
+++ run.sh
@@ -11,12 +11,12 @@ echo "-- use context"
 kubectl config use-context kind-kind
 
 
-# echo "creating cluster"
+echo "creating cluster"
 
-# kind create cluster --config=kind-ingress.yaml
+kind create cluster --config=kind-ingress.yaml
 
-# echo "loading basic go hello docker image"
-# kind load docker-image hello-go
+echo "loading basic go hello docker image"
+kind load docker-image hello-go
 
 echo "applying nginx ingress"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
