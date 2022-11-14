47467e0be97f066208afd5eb90d3fd34cd14077a
Lucas Zeng <lucasqzeng@gmail.com>
2022-11-14 16:26:45 +1100

    f

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ echo $HOME
 echo "-- installing kind"
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
 chmod +x ./kind
-sudo mv ./kind /usr/local/bin/kind
+mv ./kind /usr/local/bin/kind
 
 echo "-- getting kubectl context"
 kubectl config current-context
